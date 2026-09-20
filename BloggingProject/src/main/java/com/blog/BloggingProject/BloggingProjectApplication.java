ca450b53a061d5e4d7b1a3846fef0b6e0208d1df
Srishti Vats <amitsharma7668472886@gmail.com>
2026-09-20 14:48:45 +0530

    Database connected

---
+++ BloggingProject/src/main/java/com/blog/BloggingProject/BloggingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.blog.BloggingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingProjectApplication.class, args);
+	}
+
+}
---
+++ BloggingProject/src/test/java/com/blog/BloggingProject/BloggingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.BloggingProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
