889eed4c059256561fc2dd1fabdf2ae10731f7b7
Srishti Vats <amitsharma7668472886@gmail.com>
2026-09-25 20:49:03 +0530

    Creating and editing the post

--- BloggingProject/src/main/java/com/blog/BloggingProject/controller/PostController.java
+++ BloggingProject/src/main/java/com/blog/BloggingProject/controller/PostController.java
@@ -1,10 +1,12 @@
 package com.blog.BloggingProject.controller;
 
+import com.blog.BloggingProject.model.Post;
 import org.springframework.ui.Model;
 import com.blog.BloggingProject.repository.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -16,5 +18,16 @@ public class PostController {
     public String viewHomePage(Model model){
         model.addAttribute("post",repo.findAll());
         return "index";
+    }
+    @GetMapping("/new")
+    public String showNewPostForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "new_post";
+    }
+
+    @PostMapping("/save")
+    public String savePost(Post post) {
+        repo.save(post);
+        return "redirect:/";
     }
 }
--- BloggingProject/src/main/java/com/blog/BloggingProject/model/Post.java
+++ BloggingProject/src/main/java/com/blog/BloggingProject/model/Post.java
@@ -1,36 +1,29 @@
 package com.blog.BloggingProject.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-
 public class Post {
+
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private int id;
-    @Column(name="Title")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String title;
-    @Column(name="Content")
-    private String content;
-    @Column(name="Author")
     private String author;
+    private String content;
 
     public Post() {
     }
 
-    public Post(int id, String title, String content, String author) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.author = author;
-    }
-
-
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -42,19 +35,19 @@ public class Post {
         this.title = title;
     }
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
     public String getAuthor() {
         return author;
     }
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
